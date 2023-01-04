90fc49cd3f5f22bb6c8a2223179966a076c9ffa8
markinsantos <marcosdosantosilva@gmail.com>
2023-01-04 17:01:13 -0300

    adicionei comentario

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './main/App';
 import reportWebVitals from './reportWebVitals';
+// adicionei comentarios
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
